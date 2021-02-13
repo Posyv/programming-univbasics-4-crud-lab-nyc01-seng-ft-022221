cebe8049c90cffd8d08b627e9b31c98d342ab3c6
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-13 02:39:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   family = ["Reed", "Lily", "Posy", "Sage"]
-  family.push ("Peter")
+  family << "Peter"
 end
 
 def add_element_to_start_of_array(array, element)
